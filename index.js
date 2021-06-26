b268920cb5115399fbd50205d4ab0d1fbd52c09c
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2021-06-26 13:06:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+
 function onClickButton() {
+
+  var playerOne = prompt("Enter Player 1");
+  var playerTwo = prompt("Enter Player 2");
+
   var randomNumberOne = Math.floor(Math.random() * 6) + 1;
   var randomNumberTwo = Math.floor(Math.random() * 6) + 1;
 
   document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumberOne + ".png");
   document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumberTwo + ".png");
 
+  document.querySelector(".p1").innerHTML = playerOne;
+  document.querySelector(".p2").innerHTML = playerTwo;
+
   if (randomNumberOne > randomNumberTwo) {
-    document.querySelector("h1").innerHTML = "Player One Wins!🚩";
+    document.querySelector("h1").innerHTML = playerOne + " " + "Wins!🚩";
   } else if (randomNumberOne < randomNumberTwo) {
-    document.querySelector("h1").innerHTML = "Player Two Wins!🚩";
+    document.querySelector("h1").innerHTML = playerTwo + " " + "Wins!🚩";
   } else {
     document.querySelector("h1").innerHTML = "Draw!🤝";
   }
 }
+
